06fc3a1330f04ef79177489a786baeaa317da9b2
Lucas França <lucasfrancaid@gmail.com>
2021-06-06 06:31:24 -0300

    Change port config to run server in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const app = express();
 
-const PORT = process.env.APP_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
